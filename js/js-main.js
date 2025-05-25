f54d4801f6d5e5f0ae050b03a8da5c3cc65f9ed5
Pawel Gurlo <pawel.gurlo@gmail.com>
2025-05-25 16:06:05 +0200

    Finish basic calculator

--- js/js-main.js
+++ js/js-main.js
@@ -20,13 +20,19 @@ let operation = {
     operator: "",
     // pointer: where the user is currently typing in (left / right)
     pointer: "left",
+    // temporary solution
+    solution: "",
 
-    execute() {
+    execute(continueOperation=false) {
         if (this.verify()) {
             let temp = this.runCalculation();
             temp = this.roundExcessive(temp);
             this.reset();
-            this.a = temp;
+            this.solution = temp;
+            showSolution(this.solution);
+            if (continueOperation) {
+                this.a = temp;
+            }
             return true;
         }
         this.reset();
@@ -45,6 +51,7 @@ let operation = {
             case "-":
                 return subtract(+this.a, +this.b);
             case "x":
+            case "*":
                 return multiply(+this.a, +this.b);
             case "/":
                 return divide(+this.a, +this.b);
@@ -69,12 +76,14 @@ let operation = {
 
     addOperator(operator) {
         if (this.pointer === "left" && operator !== "=") {
+            if (this.a === "" && this.solution !== "") {
+                this.a = this.solution;
+            }
             this.operator = operator;
         }
         else {
             // calculate the solution and display it
-            if (!this.execute()) {
-                this.operator = operator;
+            if (!this.execute(true)) {
                 return false;
             }
             this.operator = operator;
@@ -82,8 +91,22 @@ let operation = {
         return true;
     },
 
+    addDecimal() {
+        if (this.pointer === "left") {
+            if (this.a.indexOf(".") === -1) {
+                this.a = (this.a === "") ? "0." : this.a + ".";
+            }
+        }
+        else {
+            if (this.b.indexOf(".") === -1) {
+                this.b = (this.b === "") ? "0." : this.b + ".";
+            }
+        }
+    },
+
     verify() {
         if (this.checkDivisionByZero()) return false;
+        if (!this.checkNumber()) return false;
         return true;
     },
 
@@ -97,19 +120,35 @@ let operation = {
         this.b = "";
         this.operator = "";
         this.pointer = "left";
+        this.solution = "";
     },
 
     roundExcessive(value, max_digits=10) {
-        return value.toPrecision(max_digits);
+        if (value.toString().length > 10) {
+            return value.toPrecision(+max_digits);
+        }
+        return value;
     },
 
     maxSizeReached() {
-        console.log(this.a.length)
         if (this.a.length >= 10 && this.pointer === "left")
             return true;
-        if (this.a.length + this.b.length >= 11 && this.pointer === "right")
+        if (this.b.length >= 10 && this.pointer === "right")
             return true;
         return false;
+    },
+
+    checkNumber() {
+        return (!isNaN(this.a) && !isNaN(this.b));
+    },
+
+    deleteLast() {
+        if (this.pointer === "right") {
+            this.b = (this.b.length > 0) ? this.b.slice(0, this.b.length-1) : this.b;
+        }
+        else if (this.pointer === "left" && this.operator === "") {
+            this.a = (this.a.length > 0) ? this.a.slice(0, this.a.length-1) : this.a;
+        }
     },
 
     // gui
@@ -121,12 +160,13 @@ let operation = {
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const equals = document.querySelector("#equals");
+const delete_btn = document.querySelector("#delete");
+const ac_btn = document.querySelector("#ac");
+const dot = document.querySelector("#dot");
 
 numbers.forEach(element => element.addEventListener(
     "click", e => {
         operation.addNumber(element.id);
-        console.log(operation);
-
         updateDisplay();
     }
 ));
@@ -141,11 +181,43 @@ operators.forEach(element => element.addEventListener(
 ));
 
 equals.addEventListener("click", e => {
-    if (operation.execute()) {
-        updateDisplay();
+    if (!operation.execute())
+        showErrorMessage();
+});
+
+dot.addEventListener("click", e => {
+    operation.addDecimal();
+    updateDisplay();
+});
+
+ac_btn.addEventListener("click", e => {
+    operation.reset();
+    updateDisplay();
+});
+
+delete_btn.addEventListener("click", e => {
+    operation.deleteLast();
+    updateDisplay();
+});
+
+// keyboard support
+document.addEventListener("keypress", e => {
+    console.log(e.key);
+    if (!isNaN(e.key)) {
+        operation.addNumber(e.key);
     }
-    else showErrorMessage();
-    
+    else if (["*", "/", "+", "-"].includes(e.key)) {
+        if (!operation.addOperator(e.key))
+            showErrorMessage();
+    }
+    else if (e.key === "." || e.key === ",") {
+        operation.addDecimal();
+    }
+    updateDisplay();
+    if (e.key === "=" || e.key === "Enter" || e.key === "Return") {
+        if (!operation.execute())
+            showErrorMessage();
+    }
 })
 
 // gui functionality
@@ -153,6 +225,10 @@ const display = document.querySelector("#text");
 
 function updateDisplay() {
     display.textContent = operation.getOperationText();
+}
+
+function showSolution(solution) {
+    display.textContent = solution;
 }
 
 function showErrorMessage() {
